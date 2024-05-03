436034068dc9d8ce6af1cb95ad786816494d73f8
jonny <nwtmman@gmail.com>
2024-05-03 04:52:26 -0700

    feat: 修改

--- src/views/Sport/index.vue
+++ src/views/Sport/index.vue
@@ -19,8 +19,6 @@ import { MultipleLangFileNameEunm, VideoMaskEnum, BottonSize } from '@/model'
                         class="sp-platform-advantages"
                     ></span>
 
-                    <span style="color: #ffffff">测试发布</span>
-
                     <van-row justify="center" class="sp-btn-wrap">
                         <botton-confirm
                             :size="BottonSize.Middle"
