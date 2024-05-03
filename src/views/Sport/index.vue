33da37b92f527a91a88ec33581976db0a4569b5e
jonny <nwtmman@gmail.com>
2024-05-03 03:53:44 -0700

    feat: 测试配置

--- src/views/Sport/index.vue
+++ src/views/Sport/index.vue
@@ -19,6 +19,8 @@ import { MultipleLangFileNameEunm, VideoMaskEnum, BottonSize } from '@/model'
                         class="sp-platform-advantages"
                     ></span>
 
+                    <span>测试发布</span>
+
                     <van-row justify="center" class="sp-btn-wrap">
                         <botton-confirm
                             :size="BottonSize.Middle"
