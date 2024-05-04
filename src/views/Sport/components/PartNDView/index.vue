64f40999afeeba87fd08750c11fdf10254fe7ff2
jonny <nwtmman@gmail.com>
2024-05-04 14:16:08 -0700

    featx: 修改

--- src/views/Sport/components/PartNDView/index.vue
+++ src/views/Sport/components/PartNDView/index.vue
@@ -7,12 +7,12 @@ import ContentView from './ContentView.vue'
 <template>
     <div>
         <div class="sp-nd-wrap">
-            <span class="sp-nd-title">{{
-                $t(MultipleLangFileNameEunm.ProductAdvantages)
-            }}</span>
-            <span class="sp-nd-text">{{
-                $t(MultipleLangFileNameEunm.ProductAdvantagesTips)
-            }}</span>
+            <div class="sp-nd-title">
+                {{ $t(MultipleLangFileNameEunm.ProductAdvantages) }}
+            </div>
+            <div class="sp-nd-text">
+                {{ $t(MultipleLangFileNameEunm.ProductAdvantagesTips) }}
+            </div>
             <content-view></content-view>
 
             <van-row justify="center" class="sp-btn-wrap">
@@ -23,25 +23,23 @@ import ContentView from './ContentView.vue'
 </template>
 
 <style scoped lang="less">
-.sp-nd-title {
-    display: block;
-    font-size: 32px;
-    color: #ff8727;
-    text-align: center;
-}
-.sp-nd-text {
-    display: block;
-    font-size: 24px;
-    color: #ffffff;
-    margin-top: 29px;
-    margin-bottom: 10px;
-}
 .sp-nd-wrap {
     padding: 75px 40px 29px 40px;
     background: url(@/assets/image/lg_part_st_bg.png) no-repeat center center,
         linear-gradient(0, #000 100%, #000 100%);
     background-position: top 150px left 50%;
     background-size: cover;
+}
+.sp-nd-title {
+    font-size: 32px;
+    color: #ff8727;
+    text-align: center;
+}
+.sp-nd-text {
+    font-size: 24px;
+    color: #ffffff;
+    margin-top: 29px;
+    margin-bottom: 10px;
 }
 .sp-btn-wrap {
     margin-top: 50px;
