05b914889755356d78648cf0b6e790895c92f0a9
jonny <nwtmman@gmail.com>
2024-05-04 14:33:24 -0700

    feat: xiugai

--- src/views/Sport/components/PartNDView/index.vue
+++ src/views/Sport/components/PartNDView/index.vue
@@ -1,4 +1,4 @@
-
+<!-- 
 <script  lang="ts" setup>
 import { MultipleLangFileNameEunm, BottonSize } from '@/model'
 import ContentView from './ContentView.vue'
@@ -44,4 +44,78 @@ import ContentView from './ContentView.vue'
 .sp-btn-wrap {
     margin-top: 50px;
 }
+</style> -->
+
+
+
+
+<script  lang="ts" setup>
+import { MultipleLangFileNameEunm, BottonSize } from '@/model'
+import ContentView from './ContentView.vue'
+</script>
+
+<template>
+    <div>
+        <div class="sf-wrap">
+            <span class="sf-content-title">{{
+                $t(MultipleLangFileNameEunm.ProductAdvantages)
+            }}</span>
+            <span class="sf-content-text">{{
+                $t(MultipleLangFileNameEunm.ProductAdvantagesTips)
+            }}</span>
+
+            <content-view></content-view>
+
+            <van-row justify="center" class="sf-content-btn">
+                <botton-confirm :size="BottonSize.Middle"></botton-confirm>
+            </van-row>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="less">
+@fontsize: 24px;
+@lineheight: 30px;
+.sf-head-wrap {
+    background: #000;
+    padding: 50px 40px 28px 40px;
+}
+.sf-head-img {
+    img {
+        width: 100%;
+        height: 454px;
+        object-fit: contain;
+        display: inline-block;
+    }
+}
+.sf-wrap {
+    padding: 75px 40px 29px 40px;
+    background: url(@/assets/image/lg_part_st_bg.png) no-repeat center center,
+        linear-gradient(0, #000 100%, #000 100%);
+    background-position: top 150px left 50%;
+    background-size: cover;
+}
+.sf-content-title {
+    display: block;
+    font-size: 32px;
+    color: #ff8727;
+    text-align: center;
+}
+.sf-content-text {
+    font-size: @fontsize;
+    line-height: @lineheight;
+    margin-top: 20px;
+    color: #ffffff;
+    display: inline-block;
+}
+.sf-content-tips {
+    font-size: @fontsize;
+    line-height: @lineheight;
+    color: #ffffff;
+    display: inline-block;
+}
+.sf-content-btn {
+    margin-top: 80px;
+}
 </style>
+
--- src/views/Sport/index.vue
+++ src/views/Sport/index.vue
@@ -1,12 +1,12 @@
 
 <script  lang="ts" setup>
 import PartSTView from './components/PartStView/index.vue'
-// import PartNDView from './components/PartNDView/index.vue'
+import PartNDView from './components/PartNDView/index.vue'
 import PartRdView from './components/PartRDView/index.vue'
 </script>
 
 <template>
     <PartSTView></PartSTView>
-    <!-- <PartNDView></PartNDView> -->
+    <PartNDView></PartNDView>
     <PartRdView></PartRdView>
 </template>
