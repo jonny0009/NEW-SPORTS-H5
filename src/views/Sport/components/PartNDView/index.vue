9bc9ce2c086397cc859660cd24e35a37e2f529e8
jonny <nwtmman@gmail.com>
2024-05-04 14:43:47 -0700

    1111

--- src/views/Sport/components/PartNDView/index.vue
+++ src/views/Sport/components/PartNDView/index.vue
@@ -1,54 +1,3 @@
-<!-- 
-<script  lang="ts" setup>
-import { MultipleLangFileNameEunm, BottonSize } from '@/model'
-import ContentView from './ContentView.vue'
-</script>
-
-<template>
-    <div>
-        <div class="sp-nd-wrap">
-            <div class="sp-nd-title">
-                {{ $t(MultipleLangFileNameEunm.ProductAdvantages) }}
-            </div>
-            <div class="sp-nd-text">
-                {{ $t(MultipleLangFileNameEunm.ProductAdvantagesTips) }}
-            </div>
-            <content-view></content-view>
-
-            <van-row justify="center" class="sp-btn-wrap">
-                <botton-confirm :size="BottonSize.Middle"></botton-confirm>
-            </van-row>
-        </div>
-    </div>
-</template>
-
-<style scoped lang="less">
-.sp-nd-wrap {
-    padding: 75px 40px 29px 40px;
-    background: url(@/assets/image/lg_part_st_bg.png) no-repeat center center,
-        linear-gradient(0, #000 100%, #000 100%);
-    background-position: top 150px left 50%;
-    background-size: cover;
-}
-.sp-nd-title {
-    font-size: 32px;
-    color: #ff8727;
-    text-align: center;
-}
-.sp-nd-text {
-    font-size: 24px;
-    color: #ffffff;
-    margin-top: 29px;
-    margin-bottom: 10px;
-}
-.sp-btn-wrap {
-    margin-top: 50px;
-}
-</style> -->
-
-
-
-
 <script  lang="ts" setup>
 import { MultipleLangFileNameEunm, BottonSize } from '@/model'
 import ContentView from './ContentView.vue'
@@ -102,8 +51,8 @@ import ContentView from './ContentView.vue'
     text-align: center;
 }
 .sf-content-text {
-    font-size: @fontsize;
-    line-height: @lineheight;
+    font-size: 24px;
+    line-height: 30px;
     margin-top: 20px;
     color: #ffffff;
     display: inline-block;
