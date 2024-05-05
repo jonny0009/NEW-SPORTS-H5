a12ee690e6e5bff66e5bc3aeb3b5efbce9d08b9c
jonny <nwtmman@gmail.com>
2024-05-04 23:35:28 -0700

    333

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import { VantResolver } from "@vant/auto-import-resolver";
 
 
 export default defineConfig({
-    base: '/start',
+    base: '/start/',
     // base: './',
     resolve: {
         alias: {
