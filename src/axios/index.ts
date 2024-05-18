932b2ff305928399fe6b6a83199fdb6c18501652
jonny <nwtmman@gmail.com>
2024-05-18 02:56:43 -0700

    axios

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,9 +1,9 @@
 // src/utils/axiosInstance.ts
 import axios from 'axios';
 const axiosInstance = axios.create({
-//   baseURL: process.env.VUE_APP_API_BASE_URL, // 从环境变量获取 API 基础 URL
-  baseURL: '/api', // 从环境变量获取 API 基础 URL
-  timeout: 10000, // 请求超时时间
+    //   baseURL: process.env.VUE_APP_API_BASE_URL, // 从环境变量获取 API 基础 URL
+    baseURL: process.env.NODE_ENV === 'production' ? '' : '/api', // 从环境变量获取 API 基础 URL
+    timeout: 10000, // 请求超时时间
 });
 export default axiosInstance;
 
@@ -11,27 +11,27 @@ export default axiosInstance;
 // ...导入 axios...
 // 请求拦截器
 axiosInstance.interceptors.request.use(
-  config => {
-    // 在这里设置请求头，例如添加 token
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+    config => {
+        // 在这里设置请求头，例如添加 token
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    error => {
+        return Promise.reject(error);
     }
-    return config;
-  },
-  error => {
-    return Promise.reject(error);
-  }
 );
 // 响应拦截器
 axiosInstance.interceptors.response.use(
-  response => {
-    // 在这里处理响应数据，例如只返回 data
-    return response.data;
-  },
-  error => {
-    // 在这里处理错误，例如统一的错误提示
-    console.error('An error occurred:', error.message);
-    return Promise.reject(error);
-  }
+    response => {
+        // 在这里处理响应数据，例如只返回 data
+        return response.data;
+    },
+    error => {
+        // 在这里处理错误，例如统一的错误提示
+        console.error('An error occurred:', error.message);
+        return Promise.reject(error);
+    }
 );
