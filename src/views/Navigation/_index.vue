eb8c65c7679d97c884555df29b4196332b06b804
jonny <nwtmman@gmail.com>
2024-05-18 03:21:40 -0700

    111

--- src/views/Navigation/_index.vue
+++ src/views/Navigation/_index.vue
@@ -1,101 +1,101 @@
 <script lang="ts" setup>
 import {
-  useTabsOptions,
-  TAB_BACK_GROUND,
-  TITLE_ACTIVE_COLOR,
-  TITLE_INACTIVE_COLOR,
-} from "./constants";
-import { computed, onMounted, ref } from "vue";
-import $ from "jquery";
+    useTabsOptions,
+    TAB_BACK_GROUND,
+    TITLE_ACTIVE_COLOR,
+    TITLE_INACTIVE_COLOR
+} from './constants'
+import { computed, onMounted, ref } from 'vue'
+import $ from 'jquery'
 import { throttle } from 'lodash'
 
-const selected = ref("");
-const sections = ref();
-const tabsOptions = computed(() => useTabsOptions());
+const selected = ref('')
+const sections = ref()
+const tabsOptions = computed(() => useTabsOptions())
 
 const onScrollTab = throttle(() => {
-  const height = $(window).scrollTop();
-  let tab = tabsOptions.value[0].key
-  sections.value.each((index, item) => {
-    if (height >= $(item).offset().top) {
-      tab = $(item).attr('id-component')
+    const height = $(window).scrollTop()
+    let tab = tabsOptions.value[0].key
+    sections.value.each((index, item) => {
+        if (height >= $(item).offset().top) {
+            tab = $(item).attr('id-component')
+        }
+    })
+    if (tab !== selected.value) {
+        selected.value = tab
     }
-  })
-  if (tab !== selected.value) {
-    selected.value = tab
-  }
-}, 50);
+}, 50)
 
 onMounted(() => {
-  // 监听滚动事件
-  $(window).scroll(onScrollTab);
-  sections.value = $(".section");
-  selected.value = tabsOptions.value[0].fileName;
-});
+    // 监听滚动事件
+    $(window).scroll(onScrollTab)
+    sections.value = $('.section')
+    selected.value = tabsOptions.value[0].fileName
+})
 const onSelectTab = ({ name }: any) => {
-  selected.value = name
-  $("html, body").animate(
-    {
-      scrollTop: $(`#${name}_componentEmelent`).offset().top,
-    },
-    300
-  );
-};
+    selected.value = name
+    $('html, body').animate(
+        {
+            scrollTop: $(`#${name}_componentEmelent`).offset().top
+        },
+        300
+    )
+}
 </script>
 
 <template>
-  <div>
-    <div class="nav-tabs-wrap">
-      <van-tabs
-        :ellipsis="false"
-        :active="selected"
-        @click-tab="onSelectTab"
-        :background="TAB_BACK_GROUND"
-        :title-active-color="TITLE_ACTIVE_COLOR"
-        :title-inactive-color="TITLE_INACTIVE_COLOR"
-      >
-        <van-tab
-          :key="item.key"
-          :name="item.key"
-          :title="item.fileName"
-          v-for="item in tabsOptions"
-        >
-        </van-tab>
-      </van-tabs>
-    </div>
+    <div>
+        <div class="nav-tabs-wrap">
+            <van-tabs
+                :ellipsis="false"
+                :active="selected"
+                @click-tab="onSelectTab"
+                :background="TAB_BACK_GROUND"
+                :title-active-color="TITLE_ACTIVE_COLOR"
+                :title-inactive-color="TITLE_INACTIVE_COLOR"
+            >
+                <van-tab
+                    :key="item.key"
+                    :name="item.key"
+                    :title="item.fileName"
+                    v-for="item in tabsOptions"
+                >
+                </van-tab>
+            </van-tabs>
+        </div>
 
-    <div
-      v-for="item in tabsOptions"
-      :id-component="item.key"
-      :key="`${item.key}_component`"
-      :id="`${item.key}_componentEmelent`"
-      class="section"
-    >
-      <component :is="item.component"></component>
+        <div
+            v-for="item in tabsOptions"
+            :id-component="item.key"
+            :key="`${item.key}_component`"
+            :id="`${item.key}_componentEmelent`"
+            class="section"
+        >
+            <component :is="item.component"></component>
+        </div>
     </div>
-  </div>
 </template>
 
 
 
 <style lang="less" scoped>
 .nav-tabs-wrap {
-  width: 100vw;
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 999;
-  background: #000;
+    width: 100vw;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 999;
+    background: #000;
 }
 .nav-tabs-container {
-  color: red;
+    color: red;
 }
 .nav-tabs-item {
-  font-size: 18px;
-  color: #fff;
+    font-size: 18px;
+    color: #fff;
 }
 :deep(.van-tabs__line) {
-  background: url("@/assets/image/nav_tab_icon.png") no-repeat;
-  background-size: cover;
+    background: url('@/assets/image/nav_tab_icon.png') no-repeat;
+    background-size: cover;
 }
 </style>
